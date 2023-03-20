55e302090df2472887bf3051837dda5b7c07c0a7
nikolainiklaus <118966948+nikolainiklaus@users.noreply.github.com>
2023-03-20 12:04:45 +0100

    CRUD for posts

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ import Express from "Express";
 import listEndpoints from "express-list-endpoints";
 import cors from "cors";
 import mongoose from "mongoose";
+import postsRouter from "./api/posts/index.js";
+import {
+  badRequestHandler,
+  notFoundHandler,
+  genericErrorHandler,
+} from "./errorHandlers.js";
 
 const server = Express();
 
@@ -14,8 +20,12 @@ server.use(cors());
 server.use(Express.json());
 
 // ENDPOINTS
+server.use("/posts", postsRouter);
 
 // ERROR HANDLERS
+server.use(badRequestHandler);
+server.use(notFoundHandler);
+server.use(genericErrorHandler);
 
 mongoose.connect(process.env.MONGO_URL);
 
